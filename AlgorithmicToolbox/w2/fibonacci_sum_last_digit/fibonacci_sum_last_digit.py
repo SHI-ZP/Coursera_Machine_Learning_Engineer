f1a0d44e8cfd591d8a55d7827dd5ccfb55bf9ba8
szp <szp268@gmail.com>
2017-08-31 10:55:13 +0800

    added the problem 6

--- AlgorithmicToolbox/w2/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ AlgorithmicToolbox/w2/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -37,15 +37,52 @@ def fibonacci_sum_last_digit(n):
             return _current_last_digit
 
 
-def main():
-    for i in range(100):
-        print(fibonacci_sum_last_digit(i))
+def fibonacci_sum_last_digit_v2(n):
+    if n <= 1:
+        return n % 10
 
+    _previous = 0
+    _current = 1
+    _sequence = []
+
+    for i in range(n + 1):
+        if 0 == i:
+            _sequence.append(0 % 10)
+        elif 1 == i:
+            _sequence.append(1 % 10)
+        else:
+            _previous, _current = _current, _previous + _current
+            _sequence.append(_current % 10)
+            if 0 == _sequence[i-1] and 1 == _sequence[i]:
+                for _ in range(2):
+                    _sequence.pop()
+                break
+
+    # _sum_sequence = sum(_sequence)
+    # _divider = int(n / len(_sequence))
+    # _remainder = n % len(_sequence)
+    # _partial_sum_sequence_1 = sum(_sequence[:_remainder - 1])
+    # _partial_sum_sequence_2 = sum(_sequence[:_remainder + 1])
+
+    # _sum_last_digit_1 = (_divider * _sum_sequence + _partial_sum_sequence_1) % 10
+    # _sum_last_digit_2 = (_divider * _sum_sequence + _partial_sum_sequence_2) % 10
+
+    return (int(n / len(_sequence)) * sum(_sequence) + sum(_sequence[:(n % len(_sequence)) + 1])) % 10
+
+
+def main():
+    print(fibonacci_sum_naive(0))
+    print(fibonacci_sum_naive(1))
+    print(fibonacci_sum_naive(2))
+
+    print(fibonacci_sum_last_digit_v2(0))
+    print(fibonacci_sum_last_digit_v2(1))
+    print(fibonacci_sum_last_digit_v2(2))
+    fibonacci_sum_last_digit_v2(99999999999991)
 
 if __name__ == '__main__':
-    main()
-    # input = sys.stdin.read()
-    # n = int(input)
-    # print(fibonacci_sum_last_digit(n))
-
-    # cProfile.run('fibonacci_sum_last_digit(n)')
+    # main()
+    input = sys.stdin.read()
+    n = int(input)
+    print(fibonacci_sum_last_digit_v2(n))
+    cProfile.run('fibonacci_sum_last_digit_v2(n)')
